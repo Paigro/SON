bd556e8d090a8abf6bac3d294fbba838e33ac73d
Paigro <paigro9@gmail.com>
2025-10-08 18:50:30 +0200

    Hoj 4 ejercicio 2.

---
+++ My project/Assets/volumeFadeOut.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class volumeFadeOut : MonoBehaviour
+{
+    [SerializeField] AudioSource audio;
+    [SerializeField] float intervalTime = 2;
+    private bool isFadingOut;
+    private float volumeMod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeMod = 1 / intervalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Programacion defensiva.
+        if (audio == null)
+        {
+            return;
+        }
+        // INPUT:
+        // Para que haga el fade out.
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            if (audio.volume > 0)
+            {
+                Debug.Log("Strt.");
+                isFadingOut = true;
+            }
+        }
+        // Para 
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            audio.volume = 1;
+        }
+
+        if (isFadingOut)
+        {
+            //Debug.Log("fding.");
+            //Debug.Log(volumeMod);
+            if (audio.volume <= 0)
+            {
+                Debug.Log("End.");
+                isFadingOut = false;
+            }
+            else
+            {
+                audio.volume -= volumeMod * Time.deltaTime;
+            }
+        }
+    }
+}
