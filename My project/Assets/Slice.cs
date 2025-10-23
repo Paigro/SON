e72e6c25ab88eebae5fa7e8874d83abc14003cb1
Paigro <paigro9@gmail.com>
2025-10-23 21:28:23 +0200

    Faltaba el casquillo.

--- My project/Assets/Slice.cs
+++ My project/Assets/Slice.cs
@@ -7,18 +7,26 @@ public class SchedEvent : MonoBehaviour
 {
     private AudioSource head; // Para reproducir los heads.
     private AudioSource tail; // Para reproducir los tail.
+    private AudioSource casing; // Para reproducir los casing.
+
     public AudioClip[] pcmDataHeads; // Clips de audio de heads.
     public AudioClip[] pcmDataTails; // Clips de audio de tails.
+    public AudioClip[] pcmDataCasing; // Clips de audio de casing.
+
     private int nHeads; // Numero de heads.
     private int nTails; // Numero de tails.
+    private int nCasing; // Numero de casing.
+
     [SerializeField] private float lap; // Intervalo temporal.
 
     void Awake()
     {
         nHeads = pcmDataHeads.Length;
         nTails = pcmDataTails.Length;
+        nCasing = pcmDataCasing.Length;
         head = gameObject.AddComponent<AudioSource>();
         tail = gameObject.AddComponent<AudioSource>();
+        casing = gameObject.AddComponent<AudioSource>();
     }
 
     void Start()
@@ -29,18 +37,18 @@ public class SchedEvent : MonoBehaviour
             // Error si el audio no es MONO.
             if (pcmDataHeads[i].channels > 1)
             {
-                Debug.LogError("NO ES MONO");
+                Debug.LogError("HEAD NO ES MONO");
                 continue;
             }
 
+            int nSamples = pcmDataHeads[i].samples;
             int nSamplesLap = (int)(pcmDataHeads[i].frequency * lap);
-            var nSamples = pcmDataHeads[i].samples;
-            var data = new float[nSamples];
+            float[] data = new float[nSamples];
 
             // Error si la duracion del lap es mayor a la del clip.
             if (nSamplesLap > nSamples)
             {
-                Debug.LogError("EL LAP ES MAYOR QUE EL CLIP");
+                Debug.LogError("EL LAP DE HEAD ES MAYOR QUE EL CLIP");
                 continue;
             }
             pcmDataHeads[i].GetData(data, 0); // Cogemos los samples.
@@ -51,25 +59,25 @@ public class SchedEvent : MonoBehaviour
         for (int i = 0; i < nTails; i++)
         {
             // Error si el audio no es MONO.
-            if (pcmDataHeads[i].channels > 1)
+            if (pcmDataTails[i].channels > 1)
             {
-                Debug.LogError("NO ES MONO");
+                Debug.LogError("TAIL NO ES MONO");
                 continue;
             }
 
+            int nSamples = pcmDataHeads[i].samples;
             int nSamplesLap = (int)(pcmDataHeads[i].frequency * lap);
-            var nSamples = pcmDataHeads[i].samples;
-            var data = new float[nSamples];
+            float[] data = new float[nSamples];
 
             // Error si la duracion del lap es mayor a la del clip.
             if (nSamplesLap > nSamples)
             {
-                Debug.LogError("EL LAP ES MAYOR QUE EL CLIP");
+                Debug.LogError("EL LAP DE TAILES MAYOR QUE EL CLIP");
                 continue;
             }
-            pcmDataHeads[i].GetData(data, 0); // Cogemos los samples.
+            pcmDataTails[i].GetData(data, 0); // Cogemos los samples.
             applyFadeIn(data, nSamplesLap); // Les aplicamos el FadeIn.
-            pcmDataHeads[i].SetData(data, 0); // Ponemos los nuevos samples en el clip.
+            pcmDataTails[i].SetData(data, 0); // Ponemos los nuevos samples en el clip.
         }
     }
     private void applyFadeIn(float[] samples, int lapSample)
@@ -99,17 +107,23 @@ public class SchedEvent : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            int h = Random.Range(0, nHeads), t = Random.Range(0, nTails);
+            int h = Random.Range(0, nHeads); // Random head.
+            int t = Random.Range(0, nTails); // Random tail.
+            int c = Random.Range(0, nCasing); // Random casing.
+
             head.clip = pcmDataHeads[h];
             tail.clip = pcmDataTails[t];
+            casing.clip = pcmDataCasing[c];
 
-            double clipLength = (double)head.clip.samples / head.pitch;
+            double headLength = (double)head.clip.samples / head.clip.frequency;
+            double tailLength = (double)tail.clip.samples / head.clip.frequency;
 
             int sRATE = AudioSettings.outputSampleRate;
-            Debug.Log($"head {h} length {clipLength}  p tail {t}  sRATE: {sRATE}");
+            Debug.Log($"head {h} length {headLength}  p tail {t}  sRATE: {sRATE}");
 
             head.Play();
-            tail.PlayScheduled(AudioSettings.dspTime + clipLength / sRATE);
+            tail.PlayScheduled(AudioSettings.dspTime + headLength - lap);
+            casing.PlayScheduled(AudioSettings.dspTime + headLength + tailLength);
         }
     }
 }
